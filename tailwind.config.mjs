60c1b00a49994123eadaf88c71a784ce078188d3
aluz <aluzdev@gmail.com>
2024-01-25 02:30:17 -0600

    dark mode works

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-import { defineConfig } from 'tailwindcss'
-import flowbite from 'flowbite/plugin'
-export default {
-	darkMode: 'class', // Add this line to enable dark mode
 
+export default {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js',
+		'./layouts/**/*.html',
+		'./content/**/*.md',
+		'./content/**/*.html',
+		'./src/**/*.js',
+	], // make sure to safelist these classes when using purge
+
+	safelist: [
+		'w-64',
+		'w-1/2',
+		'rounded-l-lg',
+		'rounded-r-lg',
+		'bg-gray-200',
+		'grid-cols-4',
+		'grid-cols-7',
+		'h-6',
+		'leading-6',
+		'h-9',
+		'leading-9',
+		'shadow-lg',
 	],
+
+	// enable dark mode via class strategy
+	darkMode: 'class',
+
 	theme: {
-		extend: {},
+		extend: {
+			// extend base Tailwind CSS utility classes
+		},
 	},
-	plugins: [require('flowbite/plugin')],
+	plugins: [
+		// include Flowbite as a plugin in your Tailwind CSS project
+		require('flowbite/plugin'),
+	],
 }
