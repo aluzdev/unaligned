f675999245f350f2a2e3a530fd5657039258043f
aluz <aluzdev@gmail.com>
2024-01-25 01:58:28 -0600

    flowbite works but its not dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import { defineConfig } from 'tailwindcss'
+import flowbite from 'flowbite/plugin'
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class', // Add this line to enable dark mode
+
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js',
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('flowbite/plugin')],
 }
